95370fee8cae32f50fb93e53b7e4836a7a9c35e2
ogoerens <ogoerens@ethz.ch>
2022-11-29 08:28:43 +0100

    Custom table name for new tables

--- Master/src/main/java/framework/DataManager.java
+++ Master/src/main/java/framework/DataManager.java
@@ -23,17 +23,27 @@ public class DataManager {
             String str = "man" + i + "/";
             String directory = System.getProperty("user.dir");
             String file = "'"+ directory+ "/" + conf.getString(str + "fileName") + "'";
+            String tbl= conf.getString(str+ "table");
+            String newTbl= conf.getString(str+ "newTable");
+            String pk = conf.getString(str+ "primaryKey");
+            String[] colTypes = conf.getStringArray(str+"types");
             String op =conf.getString(str + "operation");
             switch (op){
                 case "updateTable":
-                    String tbl= conf.getString(str+ "table");
-                    String pk = conf.getString(str+ "primaryKey");
-                    String[] colTypes = conf.getStringArray(str+"types");
-                    updateTable(tbl, pk, colTypes, file);
+                    updateTable(tbl, newTbl, pk, colTypes, file);
+                    break;
+                case "updateColumn":
+                    String column = conf.getString(str+ "column");
+                    updateColumn(tbl, newTbl, pk, column, colTypes[0], colTypes[1], file);
                     break;
                 default: System.err.println("Non-matching operation in DataManager configuration file:" +op);
             }
         }
+    }
+
+    public void updateTable(String tbl,  String primaryKey, String[] columnTypes, String dataFile){
+        String newTbl = "new"+tbl;
+        updateTable(tbl, newTbl, primaryKey,columnTypes,dataFile);
     }
 
     /**
@@ -44,7 +54,7 @@ public class DataManager {
      * @param columnTypes Must indicate the type for each column in the datafile
      * @param dataFile Can contain arbitrary number of columns. However, the first column must be FK column for tbl.
      */
-    public void updateTable(String tbl, String primaryKey, String[] columnTypes, String dataFile){
+    public void updateTable(String tbl, String newTbl, String primaryKey, String[] columnTypes, String dataFile){
         String str = "(";
         int numberOfColumns =columnTypes.length;
         for (int i=0; i<numberOfColumns;i++ ){
@@ -60,32 +70,35 @@ public class DataManager {
             //System.out.println(dataFile);
             BulkInsert qNew = new BulkInsert(dataFile,"temp1");
             qNew.update(conn);
-
             str="";
             for (int i=1; i<numberOfColumns;i++ ){
                 String corr="tbl2.corr";
                 corr+= Integer.toString(i)+" ";
                 str+= i==numberOfColumns-1? corr +" ":corr+", ";
             }
-            String newtbl = "new"+tbl;
-            String sqlStmt2= String.format("Select tbl1.*, %s into %s from %s as tbl1, temp1 as tbl2 where tbl1.%s = tbl2.corr0 ", str, newtbl, tbl ,primaryKey);
+            String sqlStmt2= String.format("Select tbl1.*, %s into %s from %s as tbl1, temp1 as tbl2 where tbl1.%s = tbl2.corr0 ", str, newTbl, tbl ,primaryKey);
             stmt.executeUpdate(sqlStmt2);
             stmt.executeUpdate("DROP TABLE temp1");
         }catch (java.sql.SQLException e){
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
 
     public void updateColumn(String tbl, String pk, String column, String keytype, String type, String dataFile){
-        try{
-           Statement stmt= conn.createStatement();
-           String sqlStmt= String.format("ALTER %s DROP COLUMN %s", tbl, column);
-           String[] typeArray ={keytype, type};
-           updateTable(tbl, pk , typeArray, dataFile);
+        String newTbl = tbl + "_" + column + "updated";
+        updateColumn(tbl, newTbl, pk, column,keytype,type,dataFile);
+    }
 
+    public void updateColumn(String tbl, String newTbl, String pk, String column, String keytype, String type, String dataFile){
+        try{
+           String[] typeArray ={keytype, type};
+           updateTable(tbl, newTbl, pk , typeArray, dataFile);
+           Statement stmt= conn.createStatement();
+           String sqlStmt= String.format("ALTER TABLE %s DROP COLUMN %s", newTbl, column);
+           stmt.executeUpdate(sqlStmt);
         }catch (java.sql.SQLException e){
-            System.out.println(e);
+            e.printStackTrace();
         }
 
     }
